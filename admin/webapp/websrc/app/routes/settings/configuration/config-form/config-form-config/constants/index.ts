ac27ffd1224beec870ec3c85773a7ed10a4344f2
Jiannan Yin <89124549+yinjn1@users.noreply.github.com>
2024-02-01 17:55:35 +0800

    Update index.ts
    fix build erroe

--- admin/webapp/websrc/app/routes/settings/configuration/config-form/config-form-config/constants/index.ts
+++ admin/webapp/websrc/app/routes/settings/configuration/config-form/config-form-config/constants/index.ts
@@ -1,5 +1,5 @@
 export * from './config-constants';
 export * from './webhook-constants';
-// export * from './proxy-constants';
+export * from './proxy-constants';
 export * from './syslog-constants';
-// export * from './ibmsa-constants';
+export * from './ibmsa-constants';
